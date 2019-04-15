87a0e0a08be0061856d29774fb15ebcb54b44025
songzongquan <songzongquan@bonc.com.cn>
2019-04-15 21:12:23 +0800

    在main.py中写了login调用的例子

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#python
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from login import *
+
+s = login('songzongquan','000000')
+
+r = getText(s,'https://code.bonc.com.cn/jira/rest/api/2/project')
+
+print(r)
