ecaff9b5f750caed64229de1d1e4e55734c2f11b
lixiaofan <lixiaofan@bonc.com.cn>
2019-04-16 20:12:58 +0800

    add

--- submitbug.py
+++ submitbug.py
@@ -9,16 +9,15 @@ def submitbug(s,summary, description):
 
     url = "https://code.bonc.com.cn/jira/rest/api/2/issue"
 
-    headers =({"Accept": "application/json","Content-Type": "application/json"})
+    headers={"Accept": "application/json","Content-Type": "application/json"}
 
-    payload=json.dumps({"fields": {"summary": summary,"issuetype": {"id": "1"},"components": [{"id": "11952"}],"project": {"id": "12017"},"description": description,"priority": {"id": "3"}}})
+    payload=json.dumps({"fields": {"summary": summary,"issuetype": {"id": "1"},"components": [{"id": "11952"}],"project": {"id": "12017"},"description": description}})
 
-    response = requests.request("POST",url,data=payload,headers=headers)
 
-    print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
-    # r= post(s,url,data=payload)
-    #
-    # print(r)
+    r= post(s,url,data=payload)
+
+
+    print(r)
 
 if __name__ == '__main__':
     s = login('lixiaofan', 'Lixiaofan123!')
