a724ce346b6b44bf90bf705303ec0abf44290bfb
lixiaofan <lixiaofan@bonc.com.cn>
2019-04-17 11:55:02 +0800

    modify

--- submitbug.py
+++ submitbug.py
@@ -2,20 +2,15 @@
 # -*- coding: UTF-8 -*-
 
 from login import *
-import requests
 import json
 
 def submitbug(s,summary, description):
 
     url = "https://code.bonc.com.cn/jira/rest/api/2/issue"
 
-    headers={"Accept": "application/json","Content-Type": "application/json"}
-
     payload=json.dumps({"fields": {"summary": summary,"issuetype": {"id": "1"},"components": [{"id": "11952"}],"project": {"id": "12017"},"description": description}})
 
-
     r= post(s,url,data=payload)
-
 
     print(r)
 
