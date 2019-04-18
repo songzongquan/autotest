7df9ef431398d981212ecfddceecf4ad93db6e18
lixiaofan <lixiaofan@bonc.com.cn>
2019-04-18 16:10:18 +0800

    add

--- screenshot.py
+++ screenshot.py
@@ -4,13 +4,13 @@ from login import *
 import os
 
 ''' 输入s指的是login()返回的s，key为要添加附件的bug的bug号，caseid为测试结果为失败的用例对应的用例编号，为了取到该用例执行是对应的图片'''
-def screenshot(s,key,caseid):
+def screenshot(s,key,id):
 
     url = "https://code.bonc.com.cn/jira/rest/api/2/issue/"+key+"/attachments"
     print(url)
     # source='./screenshot/{caseid}.png'
     basedir=os.getcwd()
-    source = basedir+'\\screenshot\\'+caseid+'.png'
+    source = basedir+'\\screenshot\\'+id+'.png'
     print(dir)
     r=uploadFile(s,url,source)
     print(r)
