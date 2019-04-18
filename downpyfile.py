1a308cd86eb9eaeae920babdd3c7e0ef86377d95
songzongquan <songzongquan@bonc.com.cn>
2019-04-18 17:22:29 +0800

    修改downpyfile.py

--- downpyfile.py
+++ downpyfile.py
@@ -1,11 +1,13 @@
 # encoding:utf-8
 from login import *
 import os
+
 def downpyfile(s,id,filename):
     url = "https://code.bonc.com.cn/jira/rest/api/2/attachment/"+id  #接口
     #print(url)
     basedir = os.getcwd()  #获取当前路径
-    path = basedir + '\\testpy\\'  #py文件存取的目录
+    path = basedir + '/testpy/'  #py文件存取的目录
+    print(path)
     if not os.path.exists(path):   #判断py文件存取的文件夹是否存在，不存在的话就新建
         #print("Selected target not exist, try to create it.")
         os.makedirs(path)
@@ -13,6 +15,15 @@ def downpyfile(s,id,filename):
     #print(target)
     d = downloadFile(s, url, target)  #调用login中的下载附件的函数
     #print(d)
+
+def downloadFiles(s,testcases):
+    for testcase in testcases:
+        fid = testcase['id']
+        fname = testcase['filename']
+        downpyfile(s,fid,fname)
+
+
+
 if __name__ == '__main__':
     s = login('0111831', '1314wy8023jc.')
     result = [{'id': '25817', 'filename': 'UntitledTestCase.py'}, {'id': '25781', 'filename': '新建文本文档.py'}]  #返回的附件列表
--- main.py
+++ main.py
@@ -2,10 +2,20 @@
 # -*- coding: UTF-8 -*-
 
 from login import *
+from gettestcase import *
+from downpyfile import *
+
+
+
+projectId = 12106
 
 cycleId = 14 #先指定一个测试循环id
 
 s = login('songzongquan','000000') #先登录jira
+testcases = gettestcase(s,cycleId) #获取测试用例执行脚本
+downloadFiles(s,testcases) #下载所有用例文件
+
+
 
 
 #r = getText(s,'https://code.bonc.com.cn/jira/rest/api/2/project')
