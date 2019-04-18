04d06533a0fa908c58c6015880834c448fbe35fd
lixiaofan <lixiaofan@bonc.com.cn>
2019-04-18 09:37:26 +0800

    add

--- screenshot.py
+++ screenshot.py
@@ -3,7 +3,8 @@
 from login import *
 import os
 
-def upload(s,key,caseid):
+''' 输入s指的是login()返回的s，key为要添加附件的bug的bug号，caseid为测试结果为失败的用例对应的用例编号，为了取到该用例执行是对应的图片'''
+def screenshot(s,key,caseid):
 
     url = "https://code.bonc.com.cn/jira/rest/api/2/issue/"+key+"/attachments"
     print(url)
@@ -16,4 +17,4 @@ def upload(s,key,caseid):
 
 if __name__ == '__main__':
     s = login('lixiaofan', 'Lixiaofan123!')
-    upload(s,'CLOUDIIP-680','680')
+    screenshot(s,'CLOUDIIP-680','680')
--- submitbug.py
+++ submitbug.py
@@ -4,15 +4,21 @@
 from login import *
 import json
 
+''' 输入s指的是login()返回的s，summary为提交的bug的主题，description为提交的bug的描述，返回bug的key值'''
 def submitbug(s,summary, description):
 
     url = "https://code.bonc.com.cn/jira/rest/api/2/issue"
 
     payload=json.dumps({"fields": {"summary": summary,"issuetype": {"id": "1"},"components": [{"id": "11952"}],"project": {"id": "12017"},"description": description}})
-
+    ''' 调用login中的post方法'''
     r= post(s,url,data=payload)
-
     print(r)
+    ''' 返回提交的bug对应的key值'''
+    dict=json.loads(r)
+    print(dict)
+    key=dict.get("key")
+    print(key)
+    return(key)
 
 if __name__ == '__main__':
     s = login('lixiaofan', 'Lixiaofan123!')
