07c0304b0a3eedbbef12608de6c02cf2fbe4e071
lixiaofan <lixiaofan@bonc.com.cn>
2019-04-19 11:45:25 +0800

    add

--- submitbug.py
+++ submitbug.py
@@ -5,11 +5,11 @@ from login import *
 import json
 
 ''' 输入s指的是login()返回的s，summary为提交的bug的主题，description为提交的bug的描述，返回bug的key值,componetsid为用例所属的模块的id，projectid为用例所属项目的id，versionid为用例所属版本的id'''
-def submitbug(s,summary, description,componetsid,projectid,versionid):
+def submitbug(s,summary, description,componentid,projectid,versionid):
 
     url = "https://code.bonc.com.cn/jira/rest/api/2/issue"
 
-    payload=json.dumps({"fields": {"summary": summary,"issuetype": {"id": "1"},"components": [{"id": componetsid}],"project": {"id": projectid},"description": description,"customfield_11303":[{"id":versionid}]}})
+    payload=json.dumps({"fields": {"summary": summary,"issuetype": {"id": "1"},"components": [{"id": componentid}],"project": {"id": projectid},"description": description,"customfield_11303":[{"id":versionid}]}})
 
     r= post(s,url,data=payload)
     print(r)
