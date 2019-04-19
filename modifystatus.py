431e624bd9a47ee50cc74716ea0b8a430ed680ec
wff <user@user-PC>
2019-04-19 15:08:49 +0800

    tijiao

--- modifystatus.py
+++ modifystatus.py
@@ -7,73 +7,75 @@ from submitbug import *
 from screenshot import *
 import os,time
 
-# 读取聪聪的CSV文件
+'''读取执行后的CSV文件,CSV文件存储在当前文件夹中的result中,从第二行开始读取'''
+
 def readCSV():
     basedir=os.getcwd()
-    p=os.path.join(basedir+'/result/zhixing.csv') #文件路径
-    d=csv.reader(open(p,'r')) # 打开CSV文件
-    # for line in f: # 逐行读取文件
-    #     d=line[:]
+    p=os.path.join(basedir+'/result/zhixing.csv')
+    d=csv.reader(open(p,'r'))
     l = list(d)    
     return l[1:]
 
-def getIssueInfo(s,issuekey):
-    url='https://code.bonc.com.cn/jira/rest/api/2/issue/'+issuekey
+'''通过issuKey(肉眼可见的用例编号如"CLOUDIIPZHYD-23"),获取issueId(从F12中获得的用例ID),versionId(版本ID,目前相当于写死的,日后会做更改),
+projectId(项目ID),componentId(模块ID),主函数调用这些ID'''
+
+def getIssueInfo(s,issueKey):
+    url='https://code.bonc.com.cn/jira/rest/api/2/issue/'+issueKey
     print(url)
     r =getJson(s,url)
-    issueid = r["id"]
-    versionid=r["fields"]["customfield_11303"][0]["id"]
-    projectid=r["fields"]["project"]["id"]
-    componentid=r["fields"]['components'][0]['id']
-    # print(issueid,versionid,projectid,componentid)
-    return (issueid,versionid,projectid,componentid)
+    issueId = r["id"]
+    versionId=r["fields"]["customfield_11303"][0]["id"]
+    projectId=r["fields"]["project"]["id"]
+    componentId=r["fields"]['components'][0]['id']
 
+    return (issueId,versionId,projectId,componentId)
 
-# 创建新执行，cycleId为测试循环ID,issueKey为测试用例ID,projectId为项目ID
-def createxcute(s,cycleId,issueid,projectId):
+'''创建新执行，通过cycleId(测试循环ID),issueId(从F12中获得的用例ID),projectId(项目ID)来获取executionId(用例执行ID)'''
+
+def creatExcute(s,cycleId,issueId,projectId):
     url='https://code.bonc.com.cn/jira/rest/zapi/latest/execution' #调“创建新执行”的接口
-    
-    values = json.dumps({"cycleId":cycleId,"issueId":issueid,"projectId":projectId})
+    values = json.dumps({"cycleId":cycleId,"issueId":issueId,"projectId":projectId})
     q= post(s,url, data=values)
-    # print(q)
     q1 = json.loads(q)
-    for k,v in q1.items(): #读取字典的key
+    for k,v in q1.items(): #读取字典的key值
         excutionId=k
-        # print(excutionId)
 
     return excutionId
 
-# 主函数，修改用例状态，excutionId为执行ID
-def modifystatus(s,cycleId,projectId):
+'''主函数(修改用例状态):先读取执行文件,然后getIssueInfo函数调用执行文件中的issueKey来获取issueId,creatExcute函数调用getIssueInfo
+中的issueId来获取executionId,最后主函数利用executionId来修改状态，用例状态为失败时调用submitbug函数提交bug，再调用screenshot函数来上传附件,'''
+
+def modifyStatus(s,cycleId,projectId):
 
     data = readCSV() # 调用ReadCSV()函数
     for d in data:
-
-        tckey = d[0]    # CSV文件共三列，tckey(用例key),status(用例状态),descr(描述)
+        issueKey = d[0]    # CSV文件目前只有三列，issueKey(用例key),status(用例状态),descr(描述)
         status = d[1]
         descr = d[2]
 
-        issue = getIssueInfo(s,tckey)
-        excutionId=createxcute(s,cycleId,issue[0],projectId) # 获取用例的执行ID，通过Createxcute()函数获得
-        url='https://code.bonc.com.cn/jira/rest/zapi/latest/execution/'+excutionId+'/execute'#
-        #print(status)
+        issue = getIssueInfo(s,issueKey) #调用getIssueInfo()函数,获取各种id
+        issueId=issue[0]
+        excutionId=createxcute(s,cycleId,issueId,projectId) # 获取用例的执行ID，通过Createxcute()函数获得
+        url='https://code.bonc.com.cn/jira/rest/zapi/latest/execution/'+excutionId+'/execute'
+
         t=-1
         if status == 'pass':
             t= 1
         elif status =='fail':
             t= 2
-            bugkey=submitbug(s,descr,descr,issue[3],projectId,issue[1])
+            versionId,componentId=issue[3],issue[1]
+            bugkey=submitbug(s,descr,descr,versionId,projectId,componentId)
             time.sleep(5)
-            screenshot(s,bugkey,tckey)
-        #print(t)
+            screenshot(s,bugkey,issueKey)
+
         values = json.dumps({"status":t})
         p= put(s,url, data=values) # 修改用例状态方法用put
-    #    print(p)
+        print(p)
 
 
 if __name__ == '__main__':
     s = login('wangyujia', 'wyj211421.')
-    modifystatus(s,'33','12106')
+    modifyStatus(s,'33','12106')
 
 
 
