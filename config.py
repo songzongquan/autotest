9a013bc92f63455c4c824399ab0590f12c4b54d1
shanghualan <shanghualan@bonc.com.cn>
2019-04-23 10:30:58 +0800

    打log

--- config.py
+++ config.py
@@ -1,31 +1,33 @@
 # -*- encoding:utf-8 -*-
-#import configparser
-
+import configparser
 import os
 from Util import Properties
+import logging
 
 def getjiraUrl():
-    properties  = getProperties()
-    jiraUrl =  properties['url']
-    return jiraUrl
-
-def getUsername():
-    properties = getProperties()
-    username = properties['username']
-    return username
-
-def getPassword():
-    properties = getProperties()
-    password =  properties['password']
-
-    return password
-
-
-def getProperties():
-    
     basedir = os.getcwd()   # 获取当前文件所在目录
     #print(basedir)
     configpath = os.path.join(basedir + '/config/config.properties')    # 获取config.properties文件所在目录
     #print(configpath)
     dictProperties = Properties(configpath).getProperties()     # 读取配置文件,字典形式返回文件内容
-    return dictProperties
+    #print(dictProperties)
+    configurl = list(dictProperties.values())[0]        # 将字典形式的值转化为列表形式，返回列表的第一个元素
+    #print(configurl)
+    return configurl
+
+if __name__ == '__main__':
+    r = getjiraUrl()
+    current_path = os.getcwd()
+    log_file = os.path.join(current_path + "/autotestlog/autotest2.log")
+    logging.basicConfig(level=logging.DEBUG,  # 控制台打印的日志级别
+                        filename=log_file,
+                        filemode='a',  ##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                        # a是追加模式，默认如果不写的话，就是追加模式
+                        format=
+                        '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
+                        # 日志格式
+                        )
+
+    logging.info(r)
+
+
--- gettestcase.py
+++ gettestcase.py
@@ -2,12 +2,17 @@
 
 from login import *
 import requests
+from config import *
+import os
+import logging
 '''
 根据测试循环id查询测试用例的附件，返回附件id与文件名
 '''
 def gettestcase(s,cycleId):
-    url='https://code.bonc.com.cn/jira/rest/zapi/latest/execution'#接口
-    url2='https://code.bonc.com.cn/jira/rest/api/2/issue'
+
+    r = getjiraUrl()
+    url=os.path.join(r+'rest/zapi/latest/execution')#接口
+    url2=os.path.join(r+'rest/api/2/issue')
     params = {'action':'expand','cycleId':cycleId}
     r = getJson(s,url,params)               #这个接口下的所有测试用例字典
 
@@ -34,4 +39,17 @@ if __name__ == '__main__':
     s = login('lixiaofan', 'Lixiaofan123!')
     r = gettestcase(s,14)
     print(r)
+
+    current_path = os.getcwd()
+    log_file = os.path.join(current_path + "/autotestlog/autotest.log")
+    logging.basicConfig(level=logging.DEBUG,  # 控制台打印的日志级别
+                        filename=log_file,
+                        filemode='a',  ##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                        # a是追加模式，默认如果不写的话，就是追加模式
+                        format=
+                        '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
+                        # 日志格式
+                        )
+
+    logging.info(r)
 
