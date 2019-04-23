2b727af8d1c6de1a39aa25ce927431269e3156fe
qiaocongcong <qiaocongcong@bonc.com.cn>
2019-04-23 16:06:59 +0800

    添加用例编写规范到readme

--- executetestcase.py
+++ executetestcase.py
@@ -2,10 +2,10 @@
 #! usr/bin/python
 
 import os
-import csv
+import csv 
+import shutil #需要使用pip install pytest-shutil安装此模块
+import logging
 
-#执行用例的名称命名和jira中对应用例保持一致
-#执行用例中判断结果,输出为:if condition:result="通过”；else:result="失败:标题:详细描述”
 
 def executetestcase():
     path = os.getcwd()  #获取当前路径
@@ -14,21 +14,21 @@ def executetestcase():
     scripts= os.listdir(script_path) #获取所有执行脚本
     length = len(scripts)  #执行脚本的个数
     result_path = os.path.join(path+"/result/")  #存放结果的路径
-    screenshot_path = os.path.join(path+"/screenshot/") #截图存放路径
-    #结果路径若不存在，新建此路径
+    screenshot_path = os.path.join(path+"/testpy/screenshot/") #截图存放路径
+    #截图路径若存在,删除后新建,若不存在,直接新建
+    if os.path.exists(screenshot_path):
+        shutil.rmtree(screenshot_path)
+        os.makedirs(screenshot_path)
+    else:
+        os.makedirs(screenshot_path)
+    #结果路径若不存在,新建此路径
     if os.path.exists(result_path):
         pass
     else:
         os.makedirs(result_path)
-    #截图路径若存在，删除后新建，若不存在，直接新建
-    if os.path.exists(screenshot_path):
-        os.rmdir(screenshot_path)
-        os.makedirs(screenshot_path)
-    else:
-        os.makedirs(screenshot_path)
     #执行结果写入csv文件
     csv_name = os.path.join(path+"/result/zhixing.csv")
-    #如果文件存在，删除
+    #如果文件存在,删除
     if os.path.exists(csv_name):
         os.remove(csv_name)
     with open(csv_name, "w") as csvfile:
