3fcf5e348cc4d6ca33415440e529d2b5df86153d
lixiaofan <lixiaofan@bonc.com.cn>
2019-04-24 14:22:42 +0800

    add

--- screenshot.py
+++ screenshot.py
@@ -2,28 +2,28 @@
 # http://docs.python-requests.org
 from login import *
 import os
-
+from log import *
 from config import *
 
-
-''' 输入s指的是login()返回的s，key为要添加附件的bug的bug号，id为测试结果为失败的用例对应的用例key值，如CLOUDIIP-23，为了取到该用例执行是对应的图片'''
+sub_logger = logging.getLogger("main.screenshot")
+# 输入s指的是login()返回的s，key为要添加附件的bug的bug号，id为测试结果为失败的用例对应的用例key值，如CLOUDIIP-23，为了取到该用例执行是对应的图片
 def screenshot(s,key,id):
 
     path = getjiraUrl()
-    # print(path)
     url = path + "rest/api/2/issue/"+key+"/attachments"
     # print(url)
+    sub_logger.debug("jira:上传附件接口的url：",url)
 
-    # url = "https://code.bonc.com.cn/jira/rest/api/2/issue/"+key+"/attachments"
-    # print(url)
     basedir=os.getcwd()
     source=os.path.join(basedir+'/screenshot/'+id+'.png')
     # print(source)
+    sub_logger.debug("用来上传的附件的地址：",source)
     if os.path.exists(source):
         r = uploadFile(s,url,source)
         # print(r)
+        sub_logger.debug("附件上传成功：",r)
     else:
-        print('不存在报错图片')
+        sub_logger.info('不存在报错图片')
 
 
 if __name__ == '__main__':
